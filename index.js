adff901caaeba9d5f8573a286fddaade3a2ef5d5
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2020-12-29 02:34:19 -0500

    Added comments on how to

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Yay comment!
 // HTML = content
 // Javascript = behavior
+
+// Can run this with either:
+// 1: node index.js
+//  OR
+// 1: Right click index.html in VS Code
+// 2: Open with Live Server
+// To stop, in the bottom bar, in the bottom right delete Port:5000
+
 console.log("Hello world!")
 console.log("And so the index begins");
