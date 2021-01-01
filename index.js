3511b5a306f322ffd8286acc71499d6cb048c093
DelgadoJosh <34148647+DelgadoJosh@users.noreply.github.com>
2021-01-01 00:23:46 -0500

    More notes

--- index.js
+++ index.js
@@ -11,3 +11,32 @@
 
 console.log("Hello world!")
 console.log("And so the index begins");
+
+// by default, it's undefined
+// let name;
+// console.log(name); // returns undefined
+
+let name = 'Bob';
+console.log(name); // Bob
+
+// Cannot be a reserved keyword
+//    let, if, var
+// Should be meaningful
+//    ie: not a, b, x, etc.
+// Cannot start with a number
+//    ex: 1name 5dvawef
+// Cannot contain a space or hypen (-)
+// Are case sensitive
+
+// let firstName = "Bobby";
+// let FirstName = "Bobbyyyyy"; // Is a different variable tahn firstName
+
+// Can have multiple defined on a single line
+// let firstName = 'Bob', lastName = 'Moss';
+
+
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(1);
+
